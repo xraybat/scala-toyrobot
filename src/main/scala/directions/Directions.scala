ff7fba629530a24524a1f00001103b3ac45605d1
xraybat <xraybat100@gmail.com>
2020-10-05 16:23:31 +1100

    DirectionsList type

--- src/main/scala/directions/Directions.scala
+++ src/main/scala/directions/Directions.scala
@@ -4,14 +4,18 @@ import scala.io.StdIn.readLine
 import scala.io.Source
 
 object Directions {
+  type DirectionsList = List[String]
 
-  def fromStdIn: List[String] = Iterator
-                                .continually(readLine)
-                                .takeWhile(Option(_).fold(false)(_.nonEmpty))
-                                .toList
+  def fromStdIn: DirectionsList =
+    Iterator
+      .continually(readLine)
+      .takeWhile(Option(_).fold(false)(_.nonEmpty))
+      .toList
 
-    // simple list pass-through for testing
-  def fromList(l: List[String]): List[String] = l
-  def fromFile(fileName: String): List[String] = Source.fromFile(fileName).getLines.toList
+  // simple list pass-through for testing
+  def fromList(l: DirectionsList): DirectionsList = l
+
+  def fromFile(fileName: String): DirectionsList =
+    Source.fromFile(fileName).getLines.toList
 
 } // Directions
