450b6f431197cd97dcfb1fbf5b812c3bfd27a830
xraybat <xraybat100@gmail.com>
2020-10-07 13:17:21 +1100

    filename CLI arg

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # scala-toy-robot
 
 scala toy robot
+
+from OS CLI:
+
+    $ sbt "run <filename>"
+    $ sbt run
+
+from within  sbt:
+
+    sbt> run <filename>
+    sbt> run
+
+and similar for testing:
+
+    $ sbt test
+    sbt> test
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,16 +2,36 @@ import board._
 import point._
 import directions._
 
-object Main extends App {
-//object Main {
-//  def main(args: Array[String]): Unit = {
+//object Main extends App {
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    if (args.length > 1) {
+      println("Too many arguments.\n"
+              + "\n$ sbt run [filename]")
+      return
+    }
+
     val b = new Board
     println(s"${b.xExtent}x${b.yExtent}")
 
     val p = new Point(0, 0)
     println(s"(${p.x}, ${p.y})")
 
-    val in = SourceDirections.fromStdIn
-    print(in)
-//  }
+    if (args.length == 0)
+      SourceDirections.fromStdIn
+    else {
+      try {
+        val in =
+          if (args.length == 0) SourceDirections.fromStdIn
+          else SourceDirections.fromFile(args(0))
+
+        println(in)
+      }
+      catch {
+        case ex: java.io.FileNotFoundException =>
+          println(s"File '${args(0)}' does not exist.")
+      }
+    } // else
+  } // main
 } // Main
