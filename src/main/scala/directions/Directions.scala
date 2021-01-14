214ebbf627909c52ff1d7a44c82d9153798f6d17
xraybat <xraybat100@gmail.com>
2021-01-14 20:25:10 +1100

    move _prefix to companion object and change name to standard complying

--- src/main/scala/directions/Directions.scala
+++ src/main/scala/directions/Directions.scala
@@ -15,36 +15,36 @@ object Directions {
 
   type ResultsList = List[String] // @TODO: first cut here??
   type ResultsListBuffer = ListBuffer[String]
+
+  private val ResultPrefix = "Robot.walk: "
 }
 
 class Directions(val directionsList: Directions.DirectionsList) {
-
-  private val _prefix = "Robot.walk: "
 
   // @MUTABLE
   private var _resultsList: Directions.ResultsListBuffer = new Directions.ResultsListBuffer()
 
   def addResult(placeRobot: PlaceRobot, inPlace: Boolean, board: Option[Board]) = {
     if (inPlace)
-      _resultsList += s"${_prefix}PLACEd at ${placeRobot.point}, ${placeRobot.orientation}"
+      _resultsList += s"${Directions.ResultPrefix}PLACEd at ${placeRobot.point}, ${placeRobot.orientation}"
     else
-      _resultsList += s"${_prefix}can't PLACE at ${placeRobot.point} on a ${board.getOrElse(new Board(0, 0))} board!"
+      _resultsList += s"${Directions.ResultPrefix}can't PLACE at ${placeRobot.point} on a ${board.getOrElse(new Board(0, 0))} board!"
   }
 
   def addResult(placeObject: PlaceObject, inPlace: Boolean) =
-    if (!inPlace) _resultsList += s"${_prefix}can't PLACE_OBJECTs until in PLACE!"
+    if (!inPlace) _resultsList += s"${Directions.ResultPrefix}can't PLACE_OBJECTs until in PLACE!"
 
   def addResult(move: Move, point: Point, board: Board) = 
     if (board.inBounds(point) && board.isBlocked(point))
-      _resultsList += s"${_prefix}can't MOVE to ${point} 'cos i'm blocked!"
+      _resultsList += s"${Directions.ResultPrefix}can't MOVE to ${point} 'cos i'm blocked!"
     else
-      _resultsList += s"${_prefix}can't MOVE to ${point} on a ${board} board 'cos it's out of bounds!"
+      _resultsList += s"${Directions.ResultPrefix}can't MOVE to ${point} on a ${board} board 'cos it's out of bounds!"
 
   def addResult(report: Report, inPlace: Boolean, point: Option[Point], orientation: Option[Orientation]) = 
     if (inPlace)
-      _resultsList += s"${_prefix}REPORTing from ${point.getOrElse(Point(-1, -1))}, ${orientation.getOrElse(Orientation.North)}"
+      _resultsList += s"${Directions.ResultPrefix}REPORTing from ${point.getOrElse(Point(-1, -1))}, ${orientation.getOrElse(Orientation.North)}"
     else
-      _resultsList += s"${_prefix}REPORTing that i'm not in PLACE!"
+      _resultsList += s"${Directions.ResultPrefix}REPORTing that i'm not in PLACE!"
 
   def printResults: Unit = _resultsList.foreach { println }
 
