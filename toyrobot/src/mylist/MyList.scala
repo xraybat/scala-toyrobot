0522bec778e86ca8b5b9fd52360dbe2c4f61b5a4
xraybat <xraybat100@gmail.com>
2021-02-19 14:42:23 +1100

    minor

--- toyrobot/src/mylist/MyList.scala
+++ toyrobot/src/mylist/MyList.scala
@@ -7,7 +7,7 @@ class MyList[T] {
   // @MUTABLE
   private var _list: ListBuffer[T] = new ListBuffer[T]
   
-  def list: List[T] = _list.toList
+  def list: List[T] = _list.toList  // immutable
   def add(that: T): Unit = _list += that
 
   override def toString: String = _list.mkString("\n")
