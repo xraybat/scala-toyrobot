519c9808423d24452c8a6c14deb66b34ae9692c9
xraybat <xraybat100@gmail.com>
2021-02-19 15:46:14 +1100

    var->val for ListBuffer; still mutable

--- toyrobot/src/mylist/MyList.scala
+++ toyrobot/src/mylist/MyList.scala
@@ -4,8 +4,8 @@ import scala.collection.mutable.ListBuffer
 
 class MyList[T] {
 
-  // @MUTABLE
-  private var _list: ListBuffer[T] = new ListBuffer[T]
+  // mutable list internally for building; still `val`, tho.
+  private val _list: ListBuffer[T] = new ListBuffer[T]
   
   def list: List[T] = _list.toList  // immutable
   def add(that: T): Unit = _list += that
